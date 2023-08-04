a523973e530ea7cf46b7fc67fef514d5b8894231
Demian143 <patrickxavier140@gmail.com>
2023-08-04 13:06:14 -0300

    Deleting comments

--- app.py
+++ app.py
@@ -3,15 +3,14 @@ from dash import Dash
 import dash_bootstrap_components as dbc
 from components.layouts.dashboard_layout import layout
 
-# Obtém o caminho completo para o arquivo styles.css
+
 css_path = os.path.join(os.path.dirname(__file__), 'assets/css/styles.css')
 
-# Criação do aplicativo Dash
 app = Dash(__name__, external_stylesheets=[
     dbc.themes.BOOTSTRAP, css_path])
 
-# Layout do dashboard
 app.layout = layout
+
 
 if __name__ == '__main__':
     app.run_server(debug=True)
