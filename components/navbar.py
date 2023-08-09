b47134a7f8ae93565c6c5723a3f0f0f9d7a6fccf
Demian143 <patrickxavier140@gmail.com>
2023-08-08 23:53:52 -0300

    Fixed #6

---
+++ components/navbar.py
@@ -0,0 +1,29 @@
+import dash_bootstrap_components as dbc
+from dash import html
+
+img = html.Img(
+    src='assets/logo.svg',
+    style={
+        "width": "15vw",
+        "padding-left": "19px"
+    })
+
+text = html.P(
+    children="DashBoard",
+    style={
+        "color": "white",
+        "align-self": "center",
+        "margin": "auto",
+        "font-size": "2.9vw"
+    })
+
+navbar = dbc.Navbar([
+    img,
+    text
+],
+    color="#333333",
+    dark=True,
+    style={
+        "border-bottom": "4px solid #008000",
+        "padding": "9px"}
+)
